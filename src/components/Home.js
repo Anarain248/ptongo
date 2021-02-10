666cc5b6a65eabedb511a73de1a69310d41cb7bd
Arjun <arjun.narain700@gmail.com>
2021-02-09 22:05:27 -0600

    server

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,24 @@
 import logo from '../arjun-logo.png'
 import Button from '@material-ui/core/Button';
 import {useState, useEffect} from 'react';
-import ExerciseList from './ExerciseList'
-import picture from '../fitness.jpg'
+import ExerciseList from './ExerciseList';
+import picture from '../fitness.jpg';
 
 const Home = () => {
     const [exercises, setExercises] = useState([
-        {name: 'Barbell Squats', reps: 'Strength: 3-5 reps, Hypertrophy: 8-12 reps, Endurance: 15-25 reps', PrimaryMuscleWorked: 'Quads, HamStrings, Glutes', type: 'compound', id: 1},
-        {name: 'Bench Press', reps: 'Strength: 3-5 reps, Hypertrophy: 8-12 reps, Endurance: 15-25 reps', PrimaryMuscleWorked: 'Chest, Shoulders, Triceps', type: 'compound', id: 2},
-        {name: 'Stiff Leg Deadlift', reps: 'Strength: 3-5 reps, Hypertrophy: 8-12 reps, Endurance: 15-25 reps', PrimaryMuscleWorked: 'HamStrings, Glutes', type: 'compound', id: 3}
-    ])
+    {name: 'Squats', reps: 'Strength: 3-5 reps, Hypertrophy: 8-12 reps, Endurance: 15-25 reps', PrimaryMuscleWorked: 'Quads, HamStrings, Glutes', type: 'compound', id: 1},
+    {name: 'Bench Press', reps: 'Strength: 3-5 reps, Hypertrophy: 8-12 reps, Endurance: 15-25 reps', PrimaryMuscleWorked: 'Chest, Shoulders, Triceps', type: 'compound', id: 2},
+    {name: 'Stiff Leg Deadlift', reps: 'Strength: 3-5 reps, Hypertrophy: 8-12 reps, Endurance: 15-25 reps', PrimaryMuscleWorked: 'HamStrings, Glutes', type: 'compound', id: 3}
+])
+
+useEffect(() => {
+    
+})
+    // const [nutrition, setNutrition] = useState([
+    //     {name: 'Chicken', category: 'Protein', macronutrients: '25 grams Protein per 4oz', calories: '250 calories'},
+    //     {name: 'Brown Rice', category: 'Carbohydrate', macronutrients: '45 grams Carbs per 1 cup', calories: '200 calories'},
+    //     {name: 'Quinoa', category: 'Carbohydrate', macronutrients: '40 grams Carbs per 1 cup', calories: '200 calories'},
+    // ])
 
     const deleteHandle = (id) => {
         const newExercises = exercises.filter(exercise => exercise.id !== id)
@@ -17,14 +26,15 @@ const Home = () => {
     }
 
 
+
+
+
     return (
         <div className="Homepage">
             <img src={logo}/>
 
-            <h3>Exercise List</h3>
-            <ExerciseList exercises={exercises} deleteHandle={deleteHandle}/> <img className="pic"src={picture}/>
-
-
+            <h3>Learn about Exercises!</h3>
+           <ExerciseList exercises={exercises} deleteHandle={deleteHandle}/> <img className="pic"src={picture}/>
         </div>
 
     );
