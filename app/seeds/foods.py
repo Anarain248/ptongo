6351122617c5b270dbaf63977b23575171defd2b
Arjun <arjun.narain700@gmail.com>
2021-02-23 15:40:09 -0600

    foodseeder

--- app/seeds/foods.py
+++ app/seeds/foods.py
@@ -7,7 +7,19 @@ def seed_food():
      totalmacros='25 grams Carbohydrates',
      totalcalories='95 calories')
 
+    data2 = Food(name='Chicken',
+     primarymacro='Carbohydrate',
+     totalmacros='25 grams Protein',
+     totalcalories='100 calories')
+
+    data3 = Food(name='Banana',
+     primarymacro='Carbohydrate',
+     totalmacros='25 grams Carbohydrates',
+     totalcalories='100 calories')
+
     db.session.add(data)
+    db.session.add(data2)
+    db.session.add(data3)
     db.session.commit()
 
 def undo_foods():
